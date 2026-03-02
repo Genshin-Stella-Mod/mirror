a15ea970d52683328861f2a891c0b25c4aed480b
Sefinek <contact@sefinek.net>
2026-03-02 14:26:17 +0100

    jwt-debug

--- passport.js
+++ passport.js
@@ -11,6 +11,12 @@ const verifyToken = async (token, req, done) => {
 	}
 
 	try {
+		// DEBUG
+		const parts = token.split('.');
+		const header = JSON.parse(Buffer.from(parts[0], 'base64url').toString());
+		console.log('[JWT Debug] header:', header);
+		console.log('[JWT Debug] secret loaded:', !!process.env.SML_JWT_SECRET, '| first 10 chars:', process.env.SML_JWT_SECRET?.substring(0, 10));
+
 		const decoded = jwt.verify(token, process.env.SML_JWT_SECRET);
 
 		console.log('============================ AUTHORIZED USER `mirror` ============================');
@@ -22,6 +28,7 @@ const verifyToken = async (token, req, done) => {
 			return done(null, false, { message: 'Token expired' });
 		}
 
+		console.log('[JWT Debug] token first 50 chars:', token.substring(0, 50));
 		console.error(err);
 		return done(null, false, { message: 'Unauthorized' });
 	}
